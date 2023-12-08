dbf15e67bcb2bbe65b7c90d38c4a9c1bf9c36f8c
danie <greenhalghdan@gmail.com>
2023-12-08 20:13:12 +0000

    first

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+from flask import Flask, render_template
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+app = Flask(__name__)
 
+@app.route('/')
+def index():
+    return render_template("index.html")
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+@app.route('/historicalposts')
+def index():
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    return render_template("histroicalposts.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
